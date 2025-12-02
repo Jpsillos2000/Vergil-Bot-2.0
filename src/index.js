ae62f6e464ae2ee77c213faf352f031c582d175d
Jpsillos2000 <jpsillos2000@gmail.com>
2025-12-02 17:15:12 -0300

    feat: add autocomplete to birthday remove command

--- src/index.js
+++ src/index.js
@@ -46,6 +46,23 @@ for (const file of eventFiles) {
 	}
 }
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isAutocomplete()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.autocomplete(interaction);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
 if (mongoUri) {
 	mongoose.connect(mongoUri)
 		.then(() => console.log('Connected to MongoDB!'))
